93e1166d7b4ec91b62cefd34b53a05d1d0e286cd
daniel-beck <beckdp01@gmail.com>
2025-01-11 15:23:26 -0500

    Initialize Domain Project and Add Resolution Entity

---
+++ Domain/Entities/Resolution.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities;
+
+public class Resolution
+{
+   public int Id { get; set; } 
+   public string? Title { get; set; }
+   public string? Description { get; set; }
+   public int Goal { get; set; }
+   public int CurrentLevel { get; set; }
+   public double PercentComplete => ((double)CurrentLevel / (double)Goal) * 100;
+}
