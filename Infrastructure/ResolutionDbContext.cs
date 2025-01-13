9297e073e5e4e5776fe83f91483acc74aac99946
daniel-beck <beckdp01@gmail.com>
2025-01-13 12:33:50 -0500

    Add Infrastructure Project and Initialize DbContext

---
+++ Infrastructure/ResolutionDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ResolutionDbContext(DbContextOptions<ResolutionDbContext> options) : DbContext(options)
+{
+    public DbSet<Resolution>? Resolutions { get; set; } = null;
+    public DbSet<ResolutionCategory>? ResolutionCategories { get; set; } = null;
+}
